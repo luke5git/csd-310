d328a8f54697ac3190d8043b32b44d3e96454971
Luke Wilkins <lawilkins52@gmail.com>
2023-07-23 14:12:37 -0500

    Wilkins -- Module 8.3 Assignments

---
+++ module_8/Wilkins -- Module 8.3 Assignment SQL Code.sql
@@ -0,0 +1,6 @@
+-- Luke Wilkins 07-23-2023
+-- CYBR410 Professor Haas
+
+INSERT INTO player(first_name, last_name, team_id) VALUES('Galaxar', 'Guardian of Moons',  1), ('Big Dipper', 'Skillet of the Galaxy', 1); 
+INSERT INTO player(first_name, last_name, team_id) VALUES('Goron', 'Destroyer of Galaxies',  2), ('Boffer', 'Eater of Worlds', 2), ('Gorb', 'Menace of the Universe', 2);
+SELECT * FROM PLAYER;
