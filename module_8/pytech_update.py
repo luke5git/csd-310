f1e0c3154fa695189beeec0c88de4d69e0981ed4
Luke Wilkins <lawilkins52@gmail.com>
2023-07-23 14:14:55 -0500

    Wilkins -- Module 8.3 -- pytech_update.py

---
+++ module_8/pytech_update.py
@@ -0,0 +1,58 @@
+#Luke Wilkins 07-23-2023 
+#Python Code for SQL connector errorcode
+
+#import libararies for mysql
+import mysql.connector
+from mysql.connector import errorcode
+#from mypassword import password
+#import mypassword
+#setting up congfig required by mysql
+config = {
+    "user": "root",
+    "password": "Pr4iS3tH310rd!",
+    "host": "127.0.0.1",
+    "database": "pysports",
+    "raise_on_warnings": True
+}
+
+#Trying to connect with mysql. If any errors are made, point them out.
+try: 
+    db = mysql.connector.connect(**config)
+
+    print("\n Database user {} connected to MySQL on host {} with database {}".format(config["user"], config["host"], config["database"]))
+
+    cursor = db.cursor()
+    print("--DISPLAYING TEAM RECORDS--")
+    cursor.execute("SELECT team_id, team_name, mascot FROM team")
+
+    teams = cursor.fetchall()
+
+    for team in teams:
+        print("Team ID: {}\nTeam Name: {}\nMascot: {}\n\n".format(team[0],team[1],team[2]))
+    cursor = db.cursor()
+    print("--DISPLAYING PLAYER RECORDS--")
+    cursor.execute("SELECT * FROM PLAYER")
+    
+    players = cursor.fetchall()
+    
+    for player in players: 
+        print("Player ID {}\nFirst Name:{}\nLast Name: {}\nTeam ID: {}\n".format(player[0],player[1],player[2],player[3]))
+
+                   
+
+
+
+
+    input("\n\n Press any key to continue...")
+
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print(" The supplies username or password are invalid")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print(" The specified database does not exist")
+    
+    else:
+        print(err)
+    
+finally:
+    db.close()
